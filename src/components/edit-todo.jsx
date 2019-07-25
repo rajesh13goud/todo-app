7dd5112000b43377be11ed10b508b1940bd4e6d1
Rajesh <rajesh.d@heptagon.in>
2019-07-25 16:42:36 +0530

    Added files

---
+++ src/components/edit-todo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class EditTodo extends Component {
+  render() {
+    return (
+      <div>
+        <p>Edit Todo-list here</p>
+      </div>
+    );
+  }
+}
+export default EditTodo;
---
+++ src/components/todos-list.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+import axios from "axios";
+class Todos extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/todos/")
+      .then(response => {
+        this.setState({ todos: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <h3>Todos List</h3>
+          <table className="table table-striped" style={{ marginTop: 20 }}>
+            <thead>
+              <tr>
+                <th>Description</th>
+                <th>Responsible</th>
+                <th>Priority</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>{this.todoList()}</tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+export default Todos;
