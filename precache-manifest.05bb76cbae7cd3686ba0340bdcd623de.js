ca56576fd0abaab7c8b95e6b66e74beec4770ab1
Rajesh <rajesh.d@heptagon.in>
2019-07-25 16:51:13 +0530

    Updates

---
+++ precache-manifest.05bb76cbae7cd3686ba0340bdcd623de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d378d8d1c8775a608f8915af73d4128",
+    "url": "/rajesh13goud/todo-app.git/index.html"
+  },
+  {
+    "revision": "02db69f210197ae0b3fc",
+    "url": "/rajesh13goud/todo-app.git/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ed3020eb41a479424f41",
+    "url": "/rajesh13goud/todo-app.git/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "02db69f210197ae0b3fc",
+    "url": "/rajesh13goud/todo-app.git/static/js/2.6284bef2.chunk.js"
+  },
+  {
+    "revision": "ed3020eb41a479424f41",
+    "url": "/rajesh13goud/todo-app.git/static/js/main.e5493f88.chunk.js"
+  },
+  {
+    "revision": "8f26ba9ffbbc77d73230",
+    "url": "/rajesh13goud/todo-app.git/static/js/runtime~main.1a5f963f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rajesh13goud/todo-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
